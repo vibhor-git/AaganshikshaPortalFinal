0fd735ece3f00b58f8ec968bd23ebdbfb90f2ba9
vibhor-git <vibhorkashyap1907github@gmail.com>
2025-05-07 09:55:47 +0530

    Fix: added __tablename__ to User model

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import validates
 
 class User(UserMixin, db.Model):
-     __tablename__ = 'users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
